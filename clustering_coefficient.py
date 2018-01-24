f918ef6f028d858b69247518f3a20d3b149eae83
eljo <eljo.go2@gmail.com>
2018-01-24 12:33:32 +0530

    Update clustering_coefficient.py

--- clustering_coefficient.py
+++ clustering_coefficient.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
+
 import networkx as nx
 
+print "-------START------"
 Ipfile = raw_input("enter input file: ")
 G = nx.read_edgelist(Ipfile, nodetype=str, delimiter="\t")
-
-
-f=open("clustering.txt","w")
-f.write(str(nx.clustering(G)))
+Opfile ="clustering.txt"
+f = open(Opfile,"w")
+clus = dict(nx.clustering(G))
+for key, value in clus.iteritems():
+    f.write('%s\t%s\n' % (key,value))
 f.close()
+
+print "-------STOP------"
